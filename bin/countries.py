09f598e799c426bd91ee45b7f84ba9a1a98f2564
psd <psd@whatfettle.com>
2015-11-10 12:11:25 +0000

    Default dates

--- bin/countries.py
+++ bin/countries.py
@@ -8,10 +8,11 @@ import sys
 
 
 column_to_field = [['The two-letter ISO 3166-1 code', 'country'],
+                   ['start-date', 'start-date'],
+                   ['end-date', 'end-date'],
                    ['Country', 'name'],
                    ['Official Name', 'official-name'],
-                   ['Name for Citizen', 'citizen-names'],
-                   ['Notes', 'text']]
+                   ['Name for Citizen', 'citizen-names']]
 
 if __name__ == '__main__':
     # the countries csv seems to be in cp1252, not utf-8
@@ -22,5 +23,7 @@ if __name__ == '__main__':
     # GB isn't included in the csv for some reason
     print ('GB', 'United Kingdom', 'The United Kingdom of Great Britain and Northern Ireland', 'Briton, British citizen', 'GB is the United Kingdom\'s ISO 3166-1 alpha-2 code. The code UK is exceptionally reserved for the United Kingdom on the request of the country. Its main usage is the .uk internet ccTLD.', sep='\t')
     for num, row in enumerate(reader):
+        row['start-date'] = ''
+        row['end-date'] = ''
         print (*[row[column] for column, field in column_to_field], sep='\t')
 
