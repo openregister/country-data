df232dcea5248b257b0d4c93679065f93fec0c91
psd <psd@whatfettle.com>
2016-02-01 13:01:54 +0000

    Trim whitespace

--- legacy/visa-fees/countries.sh
+++ legacy/visa-fees/countries.sh
@@ -2,5 +2,5 @@
 
 export LC_ALL=C
   grep '<option value="' |
-  sed -e 's/" *>/">/' -e 's/^.*">//' -e 's/<.*$//' |
+  sed -e 's/" *>/">/' -e 's/^.*">//' -e 's/<.*$//' -e 's/^ *//' -e 's/ *$//'|
   sort
